ce1f6accb9cc47e1812f4f29d1cb5d8075a88cca
beomjo <qjatjr1108@gmail.com>
2021-07-23 23:41:03 +0900

    feat: Project create

---
+++ app/src/main/java/io/beomjo/kakao/search/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.beomjo.kakao.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
