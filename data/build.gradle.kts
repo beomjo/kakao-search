12300b4b63b8f09b5203f654d7a0448857f0cc59
beomjo <qjatjr1108@gmail.com>
2021-07-28 03:15:25 +0900

    build: Data module android 설정 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -3,6 +3,16 @@ plugins {
     `detekt-setting`
 }
 
+android {
+    compileSdkVersion(Project.Config.ANDROID_COMPILE)
+
+    defaultConfig {
+        minSdkVersion(Project.Config.ANDROID_MIN)
+        targetSdkVersion(Project.Config.ANDROID_TARGET)
+    }
+}
+
+
 dependencies {
     implementation(project(":domain"))
 }
