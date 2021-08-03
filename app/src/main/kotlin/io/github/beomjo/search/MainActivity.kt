eec724b9f90b42567579a0748043878d83b1d9af
beomjo <qjatjr1108@gmail.com>
2021-08-03 16:24:03 +0900

    feat: BottomNavigation 생성

--- app/src/main/kotlin/io/github/beomjo/search/MainActivity.kt
+++ app/src/main/kotlin/io/github/beomjo/search/MainActivity.kt
@@ -2,10 +2,26 @@ package io.github.beomjo.search
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import io.github.beomjo.search.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_container_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        setupBottomNavMenu(navController)
+    }
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        bottomNav?.setupWithNavController(navController)
     }
 }
