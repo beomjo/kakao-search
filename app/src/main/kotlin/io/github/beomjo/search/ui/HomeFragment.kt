dbcbb9fec8c64722ddcfd0d8c28abaa20c958eec
beomjo <qjatjr1108@gmail.com>
2021-08-03 15:19:20 +0900

    chore: ClassName 변경

--- app/src/main/kotlin/io/github/beomjo/search/ui/HomeFragment.kt
+++ app/src/main/kotlin/io/github/beomjo/search/ui/HomeFragment.kt
@@ -4,25 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.NonNull
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import io.github.beomjo.search.databinding.FragmentCafeAndBlogBinding
+import io.github.beomjo.search.databinding.FragmentHomeBinding
 
-class CafeAndBlogFragment : Fragment() {
+class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = FragmentCafeAndBlogBinding.inflate(layoutInflater)
+        val binding = FragmentHomeBinding.inflate(layoutInflater)
         setView(binding)
         return binding.root
     }
 
-    private fun setView(binding: FragmentCafeAndBlogBinding) {
+    private fun setView(binding: FragmentHomeBinding) {
         binding.text.setOnClickListener {
-            CafeAndBlogFragmentDirections.actionCafeAndBlogDestToDetailFragment().let {
+            HomeFragmentDirections.actionCafeAndBlogDestToDetailFragment().let {
                 findNavController().navigate(it)
             }
         }
