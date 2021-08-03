2aa5ae693d292e35be328661cbe879f65386d1a8
beomjo <qjatjr1108@gmail.com>
2021-08-03 16:57:48 +0900

    feat: 화면 Orientation에 따라 layout inflate 변경

--- app/src/main/kotlin/io/github/beomjo/search/MainActivity.kt
+++ app/src/main/kotlin/io/github/beomjo/search/MainActivity.kt
@@ -2,26 +2,74 @@ package io.github.beomjo.search
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import io.github.beomjo.search.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding =
-            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_container_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        setupBottomNavMenu(navController)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navHostFragment = getNavHostFragment()
+        navController = navHostFragment.navController
+
+        setAppBarConfiguration()
+
+        setupActionBar()
+
+        setupBottomNavMenu()
+
+        setupNavigationMenu()
     }
 
-    private fun setupBottomNavMenu(navController: NavController) {
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
-        bottomNav?.setupWithNavController(navController)
+    private fun getNavHostFragment() =
+        supportFragmentManager.findFragmentById(R.id.nav_host_container_fragment) as NavHostFragment
+
+    private fun setAppBarConfiguration() {
+        appBarConfiguration = if (binding.drawerLayout != null) {
+            AppBarConfiguration(
+                setOf(R.id.home_dest, R.id.search_dest),
+                binding.drawerLayout
+            )
+        } else {
+            AppBarConfiguration(navController.graph)
+        }
+    }
+
+    private fun setupActionBar() {
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    private fun setupBottomNavMenu() {
+        binding.bottomNavView?.setupWithNavController(navController)
+    }
+
+    private fun setupNavigationMenu() {
+        binding.navView?.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        binding.navView ?: return false
+        return super.onCreateOptionsMenu(menu)
     }
 }
