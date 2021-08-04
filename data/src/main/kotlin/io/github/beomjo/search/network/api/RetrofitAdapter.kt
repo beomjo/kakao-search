185c082d8afc80a0c5010cd863f7f0303ebf85b0
beomjo <qjatjr1108@gmail.com>
2021-08-04 23:56:43 +0900

    feat: Retrofit Adapter 구현

---
+++ data/src/main/kotlin/io/github/beomjo/search/network/api/RetrofitAdapter.kt
@@ -0,0 +1,49 @@
+package io.github.beomjo.search.network.api
+
+import io.github.beomjo.search.data.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.JavaNetCookieJar
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.net.CookieManager
+import java.net.CookiePolicy
+import java.util.concurrent.TimeUnit
+
+object RetrofitAdapter {
+    private const val TIMEOUT: Long = 20
+
+    fun getInstance(baseUrl: String): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(getOkHttpClient())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private fun getOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(TIMEOUT, TimeUnit.SECONDS)
+            .writeTimeout(TIMEOUT, TimeUnit.SECONDS)
+            .readTimeout(TIMEOUT, TimeUnit.SECONDS)
+            .cookieJar(JavaNetCookieJar(getCookieManager()))
+            .addInterceptor(httpLoggingInterceptor())
+            .build()
+    }
+
+    private fun getCookieManager(): CookieManager {
+        return CookieManager().apply {
+            setCookiePolicy(CookiePolicy.ACCEPT_ALL)
+        }
+    }
+
+    private fun httpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor.Level.BODY
+            } else {
+                HttpLoggingInterceptor.Level.NONE
+            }
+        }
+    }
+}
