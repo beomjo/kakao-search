39cc9aefd778ce8a729875fced9dd2d2d3396f4b
beomjo <qjatjr1108@gmail.com>
2021-08-05 22:51:51 +0900

    chore: License Annotation 추가

--- data/src/main/kotlin/io/github/beomjo/search/DataModule.kt
+++ data/src/main/kotlin/io/github/beomjo/search/DataModule.kt
@@ -14,15 +14,7 @@
  * limitations under the License.
  */
 
-package io.github.beomjo.search.model
+package io.github.beomjo.search
 
-import com.google.gson.annotations.SerializedName
-
-internal data class ModelMeta(
-    @SerializedName("is_end")
-    val isEnd: Boolean,
-    @SerializedName("pageable_count")
-    val pageableCount: Int,
-    @SerializedName("total_count")
-    val totalCount: Int
-)
+class DataModule {
+}
