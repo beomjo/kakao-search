df3d0a16511380c0cc81001a47ce38c20e0ccb57
beomjo <qjatjr1108@gmail.com>
2021-08-05 22:45:27 +0900

    feat: DocumentApi 구현

--- data/src/main/kotlin/io/github/beomjo/search/datasource/remote/api/service/DocumentsApi.kt
+++ data/src/main/kotlin/io/github/beomjo/search/datasource/remote/api/service/DocumentsApi.kt
@@ -1,5 +1,21 @@
 package io.github.beomjo.search.datasource.remote.api.service
 
-interface DocumentsApi {
-    
+import io.github.beomjo.search.model.*
+import retrofit2.http.GET
+
+internal interface DocumentsApi {
+    @GET("search/blog")
+    suspend fun fetchBlog(): ModelDocumentList<ModelBlog>
+
+    @GET("search/cafe")
+    suspend fun fetchCafe(): ModelDocumentList<ModelCafe>
+
+    @GET("search/image")
+    suspend fun fetchImages(): ModelDocumentList<ModelImage>
+
+    @GET("search/vclip")
+    suspend fun fetchVideo(): ModelDocumentList<ModelVideo>
+
+    @GET("search/book")
+    suspend fun fetchBook(): ModelDocumentList<ModelBook>
 }
