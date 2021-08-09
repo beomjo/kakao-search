928a23af4ae60862e4cdc9144957473742a796f0
beomjo <qjatjr1108@gmail.com>
2021-08-10 01:05:27 +0900

    feat: Implements SearchPagingAdapter, DocumentViewHolder

---
+++ app/src/main/kotlin/io/github/beomjo/search/ui/adapter/viewholders/DocumentViewHolder.kt
@@ -0,0 +1,28 @@
+package io.github.beomjo.search.ui.adapter.viewholders
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import io.github.beomjo.search.databinding.DocumentListItemBinding
+import io.github.beomjo.search.entity.Document
+
+class DocumentViewHolder(
+    private val binding: DocumentListItemBinding,
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(document: Document) {
+        binding.document = document
+    }
+
+    companion object {
+        fun create(parent: ViewGroup): DocumentViewHolder {
+            return DocumentViewHolder(
+                DocumentListItemBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+        }
+    }
+}
