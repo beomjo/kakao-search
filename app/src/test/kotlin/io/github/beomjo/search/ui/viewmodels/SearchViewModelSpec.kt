1667d56821569758d0c11aee8fdaeaeb86ac572a
beomjo <qjatjr1108@gmail.com>
2021-08-31 02:35:23 +0900

    test: Amend SearchViewModel Test

--- app/src/test/kotlin/io/github/beomjo/search/ui/viewmodels/SearchViewModelSpec.kt
+++ app/src/test/kotlin/io/github/beomjo/search/ui/viewmodels/SearchViewModelSpec.kt
@@ -19,11 +19,10 @@ package io.github.beomjo.search.ui.viewmodels
 import androidx.lifecycle.Observer
 import androidx.paging.PagingData
 import androidx.paging.map
-import io.github.beomjo.search.entity.Document
-import io.github.beomjo.search.entity.DocumentType
-import io.github.beomjo.search.entity.SortType
+import io.github.beomjo.search.entity.*
 import io.github.beomjo.search.ui.paging.SearchSeparatorFactory
 import io.github.beomjo.search.ui.paging.SearchSeparatorGenerator
+import io.github.beomjo.search.usecase.GetSearchHistoryList
 import io.github.beomjo.search.usecase.GetSearchPagingData
 import io.github.beomjo.search.usecase.SearchPagingParam
 import io.kotest.core.spec.style.BehaviorSpec
@@ -39,8 +38,36 @@ import kotlinx.coroutines.flow.flowOf
 
 class SearchViewModelSpec : BehaviorSpec() {
 
+    private val getSearchPagingUseCase = mockk<GetSearchPagingData>(relaxed = true)
+
+    private val searchHistoryListUseCase = mockk<GetSearchHistoryList>(relaxed = true)
+
+    private val searchSeparatorFactory = mockk<SearchSeparatorFactory>(relaxed = true)
+
     init {
-        Given("When query, sort, documentType is given") {
+        Given("In Constructor") {
+
+            val historyList = listOf(mockk<History>())
+
+            every { searchHistoryListUseCase.invoke(Empty) } returns flowOf(historyList)
+
+
+            When("Create a view model") {
+                val viewModel = SearchViewModel(
+                    getSearchPagingUseCase,
+                    searchHistoryListUseCase,
+                    searchSeparatorFactory,
+                )
+
+                Then("History list should be loaded") {
+                    verify { searchHistoryListUseCase.invoke(eq(Empty)) }
+
+                    viewModel.history.value?.shouldContainAll(historyList)
+                }
+            }
+        }
+
+        Given("Given query, sort, documentType") {
             val query = "IU"
             val searchFilter = DocumentType.ALL
 
@@ -53,21 +80,23 @@ class SearchViewModelSpec : BehaviorSpec() {
             )
 
             val requestParamSlot = slot<SearchPagingParam>()
-            val getSearchPagingUseCase = mockk<GetSearchPagingData> {
-                every { this@mockk.invoke(capture(requestParamSlot)) } returns flowOf(pagingData)
-            }
+            every { getSearchPagingUseCase.invoke(capture(requestParamSlot)) } returns flowOf(
+                pagingData
+            )
+
+            val searchHistoryListUseCase = mockk<GetSearchHistoryList>(relaxed = true)
 
             val separatorGenerator = mockk<SearchSeparatorGenerator> {
                 every { insertTitleSeparator() } returns insertedSeparatorPagingData
                 every { insertDateSeparator() } returns insertedSeparatorPagingData
             }
-            val searchSeparatorFactory = mockk<SearchSeparatorFactory> {
-                every { create(pagingData) } returns separatorGenerator
-            }
+
+            every { searchSeparatorFactory.create(pagingData) } returns separatorGenerator
 
             val viewModel = SearchViewModel(
                 getSearchPagingUseCase,
-                searchSeparatorFactory
+                searchHistoryListUseCase,
+                searchSeparatorFactory,
             )
 
             viewModel.query.value = query
@@ -79,6 +108,9 @@ class SearchViewModelSpec : BehaviorSpec() {
             }
             viewModel.pager.observeForever(pagerObserver)
 
+            val hasFocusObserver = mockk<Observer<Boolean>>(relaxed = true)
+            viewModel.hasFocus.observeForever(hasFocusObserver)
+
             And("SortType is Title") {
                 val sortType = SortType.TITLE
                 viewModel.sort = sortType
@@ -88,6 +120,7 @@ class SearchViewModelSpec : BehaviorSpec() {
 
                     Then("Need to load PagingData") {
                         verify {
+                            hasFocusObserver.onChanged(eq(false))
                             getSearchPagingUseCase.invoke(any())
                             searchSeparatorFactory.create(eq(pagingData))
                             separatorGenerator.insertTitleSeparator()
@@ -118,6 +151,7 @@ class SearchViewModelSpec : BehaviorSpec() {
 
                     Then("Need to load PagingData") {
                         verify {
+                            hasFocusObserver.onChanged(eq(false))
                             getSearchPagingUseCase.invoke(any())
                             searchSeparatorFactory.create(eq(pagingData))
                             separatorGenerator.insertDateSeparator()
